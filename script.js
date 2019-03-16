60a6c84ee211f3c96d8a078d5a8259ecc5c71b45
nicolasguenu <nicolas.1995@hotmail.fr>
2019-03-16 20:27:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+// 	// $('#m-footer').load('footer.html');
+// 	$('#m-footer').load('footer.html');
+// });
+
+$(function () {
+	$('#header').load('header.html');
+	$('#footer').load('footer.html');
+
+	$(document).scroll(function () {
+		var $nav = $(".navbar-fixed-top");
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
+
