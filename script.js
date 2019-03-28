39bceeef704906d53d588e8fe23b7d97f3c828ed
nicolasguenu <nicolas.1995@hotmail.fr>
2019-03-28 17:06:38 +0100

    add contact page

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(function () {
 	$('#header').load('header.html');
 	$('#footer').load('footer.html');
 
+
 	$(document).scroll(function () {
 		var $nav = $(".navbar-fixed-top");
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
